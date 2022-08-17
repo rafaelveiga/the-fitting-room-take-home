7a0a4186efc97729fca0e487d3a2e7d42cbd599a
Rafael Veiga <dsg.rafael@gmail.com>
2022-08-17 00:10:42 -0300

    feat: responsive for photo placeholder

--- src/components/PhotoPlaceholder.tsx
+++ src/components/PhotoPlaceholder.tsx
@@ -1,6 +1,6 @@
 const PhotoPlaceholder = () => (
   <div className="animate-pulse height-screen">
-    <div className="grid grid-cols-5 gap-5">
+    <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-5">
       <div className="flex-1 bg-gray-200 h-96 rounded" />
       <div className="flex-1 bg-gray-200 h-96 rounded" />
       <div className="flex-1 bg-gray-200 h-96 rounded" />
